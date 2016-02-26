efda41e6ce469cee7911f78cadab6c790acde5e4
BugDiver <vinaysh@thoughtworks.com>
2016-02-26 17:09:29 +0530

    Vinay-passed all tests with generic types(Number)
    What changes have you made?
    
    * did some hard code to make all test pass with generic types. wrote
    * some fucntions which helps us to deal with generic types.
    
    Why do we need this change?
    
    * so that we can use our matrix with all Number types.
    
    What can this change impact?
    
    * ALOT. now Matrix can do a lot operation with almost all Number types.
    * but it's not a good implementation. it's very ugly. we have to find
    * some idea to make in better.

--- Matrix/src/Matrix.java
+++ Matrix/src/Matrix.java
@@ -1,45 +1,49 @@
+class Matrix<T extends Number> {
+    private int row;
+    private int col;
+    private Number[][] data;
 
-class Matrix<T extends Number>{
-	private int row;
-	private int col;
-	private Number[][] data;
-
-	public Matrix(int row ,int col){
-		this.row = row;
-		this.col =  col;
+    public Matrix(int row, int col) {
+        this.row = row;
+        this.col = col;
         this.data = new Number[row][col];
     }
-	public boolean isNullMatrix(){
-		for (int i = 0; i < row; i++){
-            for (int j = 0; j < col; j++){
-                if(data[i][j] != null){
+
+    public boolean isNullMatrix() {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (data[i][j] != null) {
                     return false;
                 }
             }
         }
         return true;
-	}
-	public void addElement(Number value , int rowPosition , int colPosition){
+    }
+
+    public void addElement(Number value, int rowPosition, int colPosition) {
         data[rowPosition][colPosition] = value;
-	}
-//
+    }
+
+    //
 //	public Number getElement(int rowPos , int colPos){
 //		return data[rowPos][colPos];
 //	}
-//    public void populateByRow(int position,int[] rowData){
-//        System.arraycopy(rowData, 0, data[position], 0, col);
-//    }
-	public boolean isEqualsTo(Matrix matrix){
-		for (int i = 0; i < row; i++){
-            for (int j = 0; j < col; j++){
-                if(!data[i][j].equals(matrix.data[i][j])){
-                	return false;
+    public void populateByRow(int position, T[] rowData) {
+        System.arraycopy(rowData, 0, data[position], 0, col);
+    }
+
+    public boolean isEqualsTo(Matrix matrix) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (!data[i][j].equals(matrix.data[i][j])) {
+                    return false;
                 }
             }
         }
         return true;
-	}
-    public String toString (){
+    }
+
+    public String toString() {
         StringBuilder sb = new StringBuilder("");
         for (Object[] aData : data) {
             for (Object anAData : aData) {
@@ -49,79 +53,120 @@ class Matrix<T extends Number>{
         }
         return sb.toString().trim();
     }
-//    private Number addNumbers(Number one, Number another) {
-//        if(one instanceof Integer){
-//            return new Integer(one.intValue() + another.intValue());
-//        }else if (one instanceof Float){
-//            return new Float(one.floatValue() + another.floatValue());
-//        }else if (one instanceof Long){
-//            return new Long(one.longValue() + another.longValue());
-//        }else if (one instanceof Double){
-//            return new Double(one.doubleValue()+ another.doubleValue());
-//
-//        }
-//        return null;
-//    }
-//	public Matrix<T> add(Matrix<T> other) {
-//        Matrix result = new Matrix(row, col);
-//        for (int i = 0; i < row; i++) {
-//            for (int j = 0; j < col; j++) {
-//
-//                Number element = addNumbers(this.data[i][j],other.data[i][j]);
-//                result.addElement(element, i, j);
-//            }
-//        }
-//        return result;
-//    }
-//
-//    public Matrix multiply(Matrix matrix) {
-//        Matrix result = new Matrix(this.row, matrix.col);
-//        for (int i = 0; i < result.row; i++){
-//            for (int j = 0; j < result.col; j++){
-//                for (int k = 0; k < col; k++){
-//                	result.data[i][j] += this.data[i][k] * matrix.data[k][j];
-//                }
-//            }
-//        }
-//        return result;
-//    }
-//
-//    public Matrix multiply(int scale) {
-//        Matrix result = new Matrix(row,col);
-//        for (int i = 0; i < result.row; i++){
-//            for (int j = 0; j < result.col; j++){
-//            	int element = data[i][j] * scale;
-//            	result.addElement(element ,i ,j);
-//            }
-//        }
-//        return result;
-//    }
-//    private Matrix getSubMatrix(int column, int rank){
-//    	Matrix subMatrix = initializeMatrix(rank-1 ,rank-1);
-//    	int subMatrixRow=0 , subMatrixCol=0;
-//    	for(int i = 1;i < rank; i++){
-//    		for(int j = 0; j < rank;j++){
-//    			if(j != column){
-//    				subMatrix.addElement(data[i][j] , subMatrixRow ,subMatrixCol++);
-//    				if(subMatrixCol % (rank-1) == 0){
-//    					subMatrixRow++;
-//    					subMatrixCol=0;
-//    				}
-//    			}
-//    		}
-//    	}
-//    	return subMatrix;
-//    }
-//    public Integer determinant(){
-//    	int result = 0, sign = 1;
-//    	if(row==1)
-//    		return data[0][0];
-//		for(int i = 0 ; i < row ; i++){
-//			Matrix subMatrix  = getSubMatrix(i ,row);
-//            result = result + data[0][i] * subMatrix.determinant() * sign;
-//			sign = -sign;
-//		}
-//    	return result;
-//    }
+
+    private void initializeWithZero(Matrix<T> matrix) {
+        for (int i = 0; i < matrix.data.length; i++) {
+            for (int j = 0; j < matrix.data[i].length; j++) {
+                matrix.data[i][j] = 0;
+            }
+        }
+    }
+
+    private Number addNumbers(Number one, Number another) {
+        if (one instanceof Integer) {
+            return one.intValue() + another.intValue();
+        } else if (one instanceof Float) {
+            return one.floatValue() + another.floatValue();
+        } else if (one instanceof Long) {
+            return one.longValue() + another.longValue();
+        } else if (one instanceof Double) {
+            return one.doubleValue() + another.doubleValue();
+        }
+        return null;
+    }
+
+    private Number multiplyNumbers(Number one, Number another) {
+        if (one instanceof Integer) {
+            return one.intValue() * another.intValue();
+        } else if (one instanceof Float) {
+            return one.floatValue() * another.floatValue();
+        } else if (one instanceof Long) {
+            return one.longValue() * another.longValue();
+        } else if (one instanceof Double) {
+            return one.doubleValue() * another.doubleValue();
+        }
+        return null;
+    }
+
+    private Number changeSign(Number one) {
+        if (one instanceof Integer) {
+            return -one.intValue();
+        } else if (one instanceof Float) {
+            return -one.floatValue();
+        } else if (one instanceof Long) {
+            return -one.longValue();
+        } else if (one instanceof Double) {
+            return -one.doubleValue();
+        }
+        return null;
+    }
+
+    public Matrix<T> add(Matrix<T> other) {
+        Matrix<T> result = new Matrix<>(row, col);
+        initializeWithZero(result);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                Number element = addNumbers(this.data[i][j], other.data[i][j]);
+                result.addElement(element, i, j);
+            }
+        }
+        return result;
+    }
+
+    public Matrix<T> multiply(Matrix matrix) {
+        Matrix<T> result = new Matrix<>(this.row, matrix.col);
+        initializeWithZero(result);
+        for (int i = 0; i < result.row; i++) {
+            for (int j = 0; j < result.col; j++) {
+                for (int k = 0; k < col; k++) {
+                    Number product = multiplyNumbers(this.data[i][k], matrix.data[k][j]);
+                    result.data[i][j] = addNumbers(result.data[i][j], product);
+                }
+            }
+        }
+        return result;
+    }
+
+    public Matrix<T> multiply(Number scale) {
+        Matrix<T> result = new Matrix<>(row, col);
+        for (int i = 0; i < result.row; i++) {
+            for (int j = 0; j < result.col; j++) {
+                Number element = multiplyNumbers(data[i][j], scale);
+                result.addElement(element, i, j);
+            }
+        }
+        return result;
+    }
+
+    private Matrix<T> getSubMatrix(int column, int rank) {
+        Matrix<T> subMatrix = new Matrix<>(rank - 1, rank - 1);
+        int subMatrixRow = 0, subMatrixCol = 0;
+        for (int i = 1; i < rank; i++) {
+            for (int j = 0; j < rank; j++) {
+                if (j != column) {
+                    subMatrix.addElement(data[i][j], subMatrixRow, subMatrixCol++);
+                    if (subMatrixCol % (rank - 1) == 0) {
+                        subMatrixRow++;
+                        subMatrixCol = 0;
+                    }
+                }
+            }
+        }
+        return subMatrix;
+    }
+
+    public Number determinant() {
+        Number result = 0;
+        Number sign = 1;
+        if (row == 1)
+            return data[0][0];
+        for (int i = 0; i < row; i++) {
+            Matrix subMatrix = getSubMatrix(i, row);
+            Number product = multiplyNumbers(data[0][i], subMatrix.determinant());
+            result = addNumbers(result, multiplyNumbers(product, sign));
+            sign = changeSign(sign);
+        }
+        return result;
+    }
 }
-	
+
--- Matrix/test/MatrixTest.java
+++ Matrix/test/MatrixTest.java
@@ -63,163 +63,162 @@ public class MatrixTest{
 
 		assertTrue(matrix1.isEqualsTo(matrix2));
 	}
-//	@Test
-//	public void testAddTwoMatrices(){
-//
-//		Matrix matrix1 = Matrix.initializeMatrix(3,3);
-//		fillMatrixWithSampleValues(matrix1);
-//
-//		Matrix matrix2 = matrix1.multiply(10);
-//
-//		Matrix addition = matrix2.add(matrix1);
-//
-//		Matrix expected = Matrix.initializeMatrix(3,3);
-//
-//		expected.addElement(11,0,0);
-//		expected.addElement(22,0,1);
-//		expected.addElement(33,0,2);
-//		expected.addElement(44,1,0);
-//		expected.addElement(55,1,1);
-//		expected.addElement(66,1,2);
-//		expected.addElement(77,2,0);
-//		expected.addElement(88,2,1);
-//		expected.addElement(99,2,2);
-//
-//		assertTrue(addition.isEqualsTo(expected));
-//
-//	}
-//	@Test
-//	public void testRepresentationOfMatrix(){
-//		Matrix matrix1 = Matrix.initializeMatrix(3,3);
-//		fillMatrixWithSampleValues(matrix1);
-//
-//		String expected  = "1 2 3 \n" +
-//						   "4 5 6 \n" +
-//						   "7 8 9";
-//		assertEquals(expected ,matrix1.toString());
-//
-//
-//	}
-//
-//
-//	@Test
-//	public void testMultiplyTwoMatrices(){
-//
-//		Matrix matrix1 = Matrix.initializeMatrix(3,3);
-//		fillMatrixWithSampleValues(matrix1);
-//
-//		Matrix matrix2 = matrix1.multiply(10);
-//
-//		Matrix product = matrix2.multiply(matrix1);
-//
-//		Matrix expected = Matrix.initializeMatrix(3,3);
-//
-//		expected.addElement(300,0,0);
-//		expected.addElement(360,0,1);
-//		expected.addElement(420,0,2);
-//		expected.addElement(660,1,0);
-//		expected.addElement(810,1,1);
-//		expected.addElement(960,1,2);
-//		expected.addElement(1020,2,0);
-//		expected.addElement(1260,2,1);
-//		expected.addElement(1500,2,2);
-//
-//		assertTrue(product.isEqualsTo(expected));
-//	}
-//	@Test
-//	public void testScalarMultiplication(){
-//
-//		Matrix matrix1 = Matrix.initializeMatrix(3,3);
-//		fillMatrixWithSampleValues(matrix1);
-//
-//		Matrix scaledMatrix = matrix1.multiply(10);
-//
-//		Matrix expected = Matrix.initializeMatrix(3,3);
-//
-//		expected.addElement(10,0,0);
-//		expected.addElement(20,0,1);
-//		expected.addElement(30,0,2);
-//		expected.addElement(40,1,0);
-//		expected.addElement(50,1,1);
-//		expected.addElement(60,1,2);
-//		expected.addElement(70,2,0);
-//		expected.addElement(80,2,1);
-//		expected.addElement(90,2,2);
-//
-//		assertTrue(scaledMatrix.isEqualsTo(expected));
-//	}
-//	@Test
-//	public void testDeterminantOfMatrix(){
-//		Matrix matrix1 = Matrix.initializeMatrix(3,3);
-//		fillMatrixWithSampleValues(matrix1);
-//
-//		int determinant = matrix1.determinant();
-//		assertEquals(determinant , 0);
-//	}
-//
-//	@Test
-//	public void determinantTest4by4() {
-//		Matrix matrix = Matrix.initializeMatrix(4,4);
-//		int[] rowFirst = {1,2,3,4};
-//		int[] rowSecond = {2,3,4,5};
-//		int[] rowThird = {2,3,4,7};
-//		int[] rowFourth = {2,3,2,3};
-//
-//		matrix.populateByRow(0,rowFirst);
-//		matrix.populateByRow(1,rowSecond);
-//		matrix.populateByRow(2,rowThird);
-//		matrix.populateByRow(3, rowFourth);
-//
-//
-//		int determinant = matrix.determinant();
-//		assertEquals(-4,determinant);
-//	}
-//
-//	@Test
-//	public void determinantTest5by5() {
-//		Matrix matrix = Matrix.initializeMatrix(5,5);
-//		int[] rowFirst = {1,2,3,4,3};
-//		int[] rowSecond = {2,3,4,5,4};
-//		int[] rowThird = {2,3,4,7,5};
-//		int[] rowFourth = {2,3,2,3,6};
-//		int[] rowFifth = {1,2,3,0,7};
-//
-//		matrix.populateByRow(0,rowFirst);
-//		matrix.populateByRow(1,rowSecond);
-//		matrix.populateByRow(2,rowThird);
-//		matrix.populateByRow(3, rowFourth);
-//		matrix.populateByRow(4,rowFifth);
-//
-//		int determinant = matrix.determinant();
-//		assertEquals(-24,determinant);
-//	}
-//
-//	@Test
-//	public void determinantTest10by10() {
-//		Matrix matrix = Matrix.initializeMatrix(10,10);
-//		int[] rowFirst = {1,2,3,4,3,2,3,4,5,6};
-//		int[] rowSecond = {2,3,4,5,4,1,1,4,6,7};
-//		int[] rowThird = {2,3,4,7,5,0,0,4,5,6};
-//		int[] rowFourth = {2,3,2,3,6,9,6,7,4,2};
-//		int[] rowFifth = {1,2,3,0,7,6,4,3,2,1};
-//		int[] rowSixth = {1,2,3,2,3,4,5,3,4,5 };
-//		int[] rowSeventh = {1,2,3,4,5,6,7,8,9,2};
-//		int[] rowEighth = {2,3,4,2,3,5,6,0,0,1};
-//		int[] rowNinth = {1,1,1,2,3,6,5,4,5,8};
-//		int[] rowTenth = {2,3,4,5,3,1,2,1,3,2};
-//
-//		matrix.populateByRow(0,rowFirst);
-//		matrix.populateByRow(1,rowSecond);
-//		matrix.populateByRow(2,rowThird);
-//		matrix.populateByRow(3, rowFourth);
-//		matrix.populateByRow(4,rowFifth);
-//		matrix.populateByRow(5,rowSixth);
-//		matrix.populateByRow(6,rowSeventh);
-//		matrix.populateByRow(7,rowEighth);
-//		matrix.populateByRow(8,rowNinth);
-//		matrix.populateByRow(9,rowTenth);
-//
-//		int determinant = matrix.determinant();
-//		assertEquals(4464,determinant);
-//	}
+	@Test
+	public void testAddTwoMatrices(){
+
+		Matrix<Integer> matrix1 =new Matrix<>(3,3);
+		fillMatrixWithSampleValues(matrix1);
+
+		Matrix<Integer> matrix2 = matrix1.multiply(10);
+
+		Matrix addition = matrix2.add(matrix1);
+
+		Matrix<Integer> expected =new Matrix<>(3,3);
+
+		expected.addElement(11,0,0);
+		expected.addElement(22,0,1);
+		expected.addElement(33,0,2);
+		expected.addElement(44,1,0);
+		expected.addElement(55,1,1);
+		expected.addElement(66,1,2);
+		expected.addElement(77,2,0);
+		expected.addElement(88,2,1);
+		expected.addElement(99,2,2);
+
+		assertTrue(addition.isEqualsTo(expected));
+
+	}
+	@Test
+	public void testRepresentationOfMatrix(){
+		Matrix<Integer> matrix1 =new Matrix<>(3,3);
+		fillMatrixWithSampleValues(matrix1);
+
+		String expected  = "1 2 3 \n" +
+						   "4 5 6 \n" +
+						   "7 8 9";
+		assertEquals(expected ,matrix1.toString());
+	}
+
+
+	@Test
+	public void testMultiplyTwoMatrices(){
+
+		Matrix<Integer> matrix1 =new  Matrix<>(3,3);
+		fillMatrixWithSampleValues(matrix1);
+
+		Matrix<Integer> matrix2 = matrix1.multiply(10);
+
+		Matrix<Integer> product = matrix2.multiply(matrix1);
+
+		Matrix<Integer> expected =new  Matrix<>(3,3);
+
+		expected.addElement(300,0,0);
+		expected.addElement(360,0,1);
+		expected.addElement(420,0,2);
+		expected.addElement(660,1,0);
+		expected.addElement(810,1,1);
+		expected.addElement(960,1,2);
+		expected.addElement(1020,2,0);
+		expected.addElement(1260,2,1);
+		expected.addElement(1500,2,2);
+
+		assertTrue(product.isEqualsTo(expected));
+	}
+	@Test
+	public void testScalarMultiplication(){
+
+		Matrix<Integer> matrix1 =new  Matrix<>(3,3);
+		fillMatrixWithSampleValues(matrix1);
+
+		Matrix<Integer> scaledMatrix = matrix1.multiply(10);
+
+		Matrix<Integer> expected =new  Matrix<>(3,3);
+
+		expected.addElement(10,0,0);
+		expected.addElement(20,0,1);
+		expected.addElement(30,0,2);
+		expected.addElement(40,1,0);
+		expected.addElement(50,1,1);
+		expected.addElement(60,1,2);
+		expected.addElement(70,2,0);
+		expected.addElement(80,2,1);
+		expected.addElement(90,2,2);
+
+		assertTrue(scaledMatrix.isEqualsTo(expected));
+	}
+	@Test
+	public void testDeterminantOfMatrix(){
+		Matrix<Integer> matrix1 =new Matrix<>(3,3);
+		fillMatrixWithSampleValues(matrix1);
+
+		int determinant = (int)matrix1.determinant();
+		assertEquals(determinant , 0);
+	}
+
+	@Test
+	public void determinantTest4by4() {
+		Matrix<Integer> matrix = new Matrix<>(4,4);
+		Integer[] rowFirst = {1,2,3,4};
+		Integer[] rowSecond = {2,3,4,5};
+		Integer[] rowThird = {2,3,4,7};
+		Integer[] rowFourth = {2,3,2,3};
+
+		matrix.populateByRow(0,rowFirst);
+		matrix.populateByRow(1,rowSecond);
+		matrix.populateByRow(2,rowThird);
+		matrix.populateByRow(3, rowFourth);
+
+
+		int determinant =(int) matrix.determinant();
+		assertEquals(-4,determinant);
+	}
+
+	@Test
+	public void determinantTest5by5() {
+		Matrix<Integer> matrix =new Matrix<>(5,5);
+		Integer[] rowFirst = {1,2,3,4,3};
+		Integer[] rowSecond = {2,3,4,5,4};
+		Integer[] rowThird = {2,3,4,7,5};
+		Integer[] rowFourth = {2,3,2,3,6};
+		Integer[] rowFifth = {1,2,3,0,7};
+
+		matrix.populateByRow(0,rowFirst);
+		matrix.populateByRow(1,rowSecond);
+		matrix.populateByRow(2,rowThird);
+		matrix.populateByRow(3, rowFourth);
+		matrix.populateByRow(4,rowFifth);
+
+		int determinant =(int) matrix.determinant();
+		assertEquals(-24,determinant);
+	}
+
+	@Test
+	public void determinantTest10by10() {
+		Matrix<Integer> matrix =new Matrix<>(10,10);
+
+		Integer[] rowFirst = {1,2,3,4,3,2,3,4,5,6};
+		Integer[] rowSecond = {2,3,4,5,4,1,1,4,6,7};
+		Integer[] rowThird = {2,3,4,7,5,0,0,4,5,6};
+		Integer[] rowFourth = {2,3,2,3,6,9,6,7,4,2};
+		Integer[] rowFifth = {1,2,3,0,7,6,4,3,2,1};
+		Integer[] rowSixth = {1,2,3,2,3,4,5,3,4,5 };
+		Integer[] rowSeventh = {1,2,3,4,5,6,7,8,9,2};
+		Integer[] rowEighth = {2,3,4,2,3,5,6,0,0,1};
+		Integer[] rowNinth = {1,1,1,2,3,6,5,4,5,8};
+		Integer[] rowTenth = {2,3,4,5,3,1,2,1,3,2};
+
+		matrix.populateByRow(0,rowFirst);
+		matrix.populateByRow(1,rowSecond);
+		matrix.populateByRow(2,rowThird);
+		matrix.populateByRow(3, rowFourth);
+		matrix.populateByRow(4,rowFifth);
+		matrix.populateByRow(5,rowSixth);
+		matrix.populateByRow(6,rowSeventh);
+		matrix.populateByRow(7,rowEighth);
+		matrix.populateByRow(8,rowNinth);
+		matrix.populateByRow(9,rowTenth);
+
+		int determinant = (int)matrix.determinant();
+		assertEquals(4464,determinant);
+	}
 }
